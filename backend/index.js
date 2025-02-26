b2bafe9b3b17079780289a2692f11eea10eabe5c
Dhruv <dhruvgoyal014@gmail.com>
2025-02-26 17:54:23 +0530

    socket implemented

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ import connectDB from './config/connectDb.js';
 import userRoutes from './routes/user.routes.js';
 import chatRoutes from './routes/chat.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import { Server } from 'socket.io';
+import http from 'http'
+
 dotenv.config()
 const app = express();
 
@@ -17,6 +20,50 @@ app.use('/v1/api/chat', chatRoutes)
 app.use('/v1/api/messages', messageRoutes)
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+io.on('connection', (socket) => {
+
+    socket.on('setup', (userData) => {
+        socket.join(userData?.id)
+        socket.emit('connected')
+    })
+
+    socket.on('join chat', (room) => {
+        socket.join(room)
+    })
+
+    socket.on('typing', (room) => {
+        socket.in(room).emit('typing')
+    })
+
+    socket.on('stopped typing', (room) => {
+        socket.in(room).emit('stopped typing')
+    })
+
+    socket.on('new message', (message) => {
+        let chat = message.chat
+        if (!chat.users) return console.log('Chat user not defined')
+
+
+        chat.users.forEach((user) => {
+            if (user._id === message.sender?._id) return;
+            socket.in(user._id).emit('message received', message)
+        })
+    })
+
+    socket.off('setup', () => {
+        console.log('Client disconnected')
+        socket.leave(userData?._id)
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
--- frontend/src/components/Chats/SingleChat.jsx
+++ frontend/src/components/Chats/SingleChat.jsx
@@ -9,6 +9,12 @@ import Loader from '../loader/Loader'
 import toast from 'react-hot-toast';
 import { getAllMessages, sendNewMessage } from '../../services/messageApi';
 import ScrollableChat from './ScrollableChat';
+import io from 'socket.io-client'
+import Lottie from 'react-lottie'
+import AnimationData from '../../animations/typing.json'
+
+const ENDPOINT = 'http://localhost:8080';
+let socket, selectedChatCompare;
 
 function SingleChat({ fetchAgain, setFetchAgain }) {
 
@@ -17,6 +23,35 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
     const [messages, setMessages] = useState([]);
     const [loading, setLoading] = useState(false);
     const [newMessage, setNewMessage] = useState('');
+    const [socketConnected, setSocketConnected] = useState(false)
+    const [typing, setTyping] = useState(false)
+    const [isTyping, setIsTyping] = useState(false)
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: AnimationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
+
+    useEffect(() => {
+        socket = io(ENDPOINT)
+        socket.emit('setup', user);
+        socket.on('connected', () => {
+            console.log("Connected to the server");
+            setSocketConnected(true);
+        });
+        socket.on('typing', () => {
+            setIsTyping(true);
+        })
+
+        socket.on('stopped typing', () => {
+            setIsTyping(false);
+        })
+
+    }, [])
 
     const fetchMessages = async () => {
         if (!selectedChat) return;
@@ -24,6 +59,8 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
         try {
             const { data } = await getAllMessages(selectedChat._id);
             setMessages(data);
+
+            socket.emit('join chat', selectedChat?._id)
         } catch (error) {
             toast.error("Failed to load the messages");
             console.error(error);
@@ -34,12 +71,24 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
 
     useEffect(() => {
         fetchMessages();
+
+        selectedChatCompare = selectedChat;
     }, [selectedChat])
 
-    console.log(messages)
+    useEffect(() => {
+        socket.on('message received', (message) => {
+            if (!selectedChatCompare || selectedChatCompare._id !== message?.chat?._id) {
+                // give notification
+            }
+            else {
+                setMessages([...messages, message])
+            }
+        })
+    })
 
     const sendMessage = async (e) => {
         if (e.key === "Enter" && newMessage) {
+            socket.emit("stopped typing", selectedChat?._id)
             try {
                 setNewMessage("");
                 const formData = {
@@ -47,6 +96,7 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
                     content: newMessage,
                 }
                 const { data } = await sendNewMessage(formData);
+                socket.emit('new message', data)
                 setMessages([...messages, data]);
             } catch (error) {
                 toast.error("Failed to send the message");
@@ -57,6 +107,22 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
 
     const typingHandler = (e) => {
         setNewMessage(e.target.value)
+
+        if (!socketConnected) return;
+        if (!typing) {
+            setTyping(true)
+            socket.emit('typing', selectedChat?._id);
+        }
+
+        let lastTypingTime = new Date().getTime();
+        setTimeout(() => {
+            let timeNow = new Date().getTime()
+            let timeDiff = timeNow - lastTypingTime
+            if (timeDiff >= 3000 && typing) {
+                socket.emit('stopped typing', selectedChat?._id);
+                setTyping(false);
+            }
+        }, 3000)
     }
 
     return (
@@ -90,6 +156,13 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
                                     <ScrollableChat messages={messages} />
                                 </div>
                                 <FormControl onKeyDown={sendMessage}>
+                                    {isTyping &&
+                                        <Lottie
+                                            options={defaultOptions}
+                                            width={70}
+                                            style={{ marginBottom: 10, marginLeft: 0 }}
+                                        />
+                                    }
                                     <input
                                         placeholder="Type a message..."
                                         value={newMessage}
